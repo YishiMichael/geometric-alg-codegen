2a5b20a712f8580e6d873f218378c7ac17d61119
YishiMichael <50232075+YishiMichael@users.noreply.github.com>
2026-03-03 00:16:22 -0800

    chore: untrack powershell script

--- generate.ps1
+++
@@ -1,9 +0,0 @@
-Get-ChildItem ./src/algebra/*.txt | ForEach-Object {
-    $name = $_.BaseName
-
-    ./target/debug/geometric-alg-codegen `
-        --rust "./src/generated/rust/$name.rs" `
-        --wgsl "./src/generated/wgsl/$name.wgsl" `
-        --glsl "./src/generated/glsl/$name.glsl" `
-        -- "./src/algebra/$name.txt"
-}
